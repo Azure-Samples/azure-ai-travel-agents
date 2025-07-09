895be9deed8ce43a4cf83a17e883bd21ae9ac3d0
Pamela Fox <pamela.fox@gmail.com>
2025-07-09 11:14:58 -0700

    Get streamable app working

--- src/tools/itinerary-planning/src/app.py
+++ src/tools/itinerary-planning/src/app.py
@@ -1,27 +1,19 @@
 import logging
 
 import uvicorn
-from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
-from starlette.routing import Mount, Route
+from starlette.routing import Route
 
-from mcp_server import itinerary_mcp
+from mcp_server import mcp
 
 logger = logging.getLogger(__name__)
 
-# Not strictly necessary, but demonstrates the inclusion of non-MCP routes
+# Not necessary for MCP, but demonstrates the inclusion of non-MCP routes
 async def homepage(request):
     return HTMLResponse("Itinerary planning MCP server")
 
-
-app = Starlette(
-    debug=True,
-    routes=[
-        Route("/", endpoint=homepage),
-        Mount("/mcp", app=itinerary_mcp.streamable_http_app())
-    ]
-)
-
+app = mcp.streamable_http_app()
+app.router.routes.append(Route("/", endpoint=homepage))
 
 def run():
     """Start the Starlette server"""
